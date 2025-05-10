8960be32c19a345c079017981011e677349224e7
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-10 21:23:46 +0530

    Update 2877. Create a DataFrame from List.py

--- 2877. Create a DataFrame from List.py
+++ 2877. Create a DataFrame from List.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     column=["student_id","age"]
     result=pd.DataFrame(student_data,columns=column)
